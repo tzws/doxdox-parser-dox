46c211403cb60da158cb01fa1d8deca649f0ed02
Scott Doxey <neogeek0101@gmail.com>
2016-10-18 08:36:29 -0400

    Whitespace fix.

--- index.js
+++ index.js
@@ -10,80 +10,83 @@ const formatStringForUID = content =>
         .replace(/[^\w\.]+/g, '-')
         .replace(/^-|-$/g, '');
 
-const parser = (content, filename) => dox.parseComments(content, {'raw': true}).reduce((prev, curr) => {
+const parser = (content, filename) =>
+    dox.parseComments(content, {'raw': true}).reduce((prev, curr) => {
 
-    if (!curr.ignore && curr.ctx) {
+        if (!curr.ignore && curr.ctx) {
 
-        prev.push({
-            'uid': formatStringForUID(`${filename}-${curr.ctx.string}`),
-            'isPrivate': curr.isPrivate,
-            'type': curr.ctx.type,
-            'name': formatStringForName(curr.ctx.string),
-            'description': curr.description.full,
-            'params': curr.tags.filter(tag => tag.type === 'param' && !tag.name.match(/\./))
-                .reduce((prev, curr) => {
+            prev.push({
+                'uid': formatStringForUID(`${filename}-${curr.ctx.string}`),
+                'isPrivate': curr.isPrivate,
+                'type': curr.ctx.type,
+                'name': formatStringForName(curr.ctx.string),
+                'description': curr.description.full,
+                'params': curr.tags.filter(tag =>
+                    tag.type === 'param' && !tag.name.match(/\./))
+                        .reduce((prev, curr) => {
 
-                    if (prev) {
+                            if (prev) {
 
-                        return `${prev}, ${curr.name.replace(/\[|\]/g, '')}`;
+                                return `${prev}, ${curr.name.replace(/\[|\]/g, '')}`;
 
-                    }
+                            }
 
-                    return `${curr.name.replace(/\[|\]/g, '')}`;
+                            return `${curr.name.replace(/\[|\]/g, '')}`;
 
-                }, ''),
-            'tags': {
-                'example': curr.tags.filter(tag => tag.type === 'example')
-                    .reduce((prev, curr) => {
+                        }, ''),
+                'tags': {
+                    'example': curr.tags.filter(tag => tag.type === 'example')
+                        .reduce((prev, curr) => {
 
-                        prev.push(curr.string);
+                            prev.push(curr.string);
 
-                        return prev;
+                            return prev;
 
-                    }, []),
-                'param': curr.tags.filter(tag => tag.type === 'param')
-                    .reduce((prev, curr) => {
+                        }, []),
+                    'param': curr.tags.filter(tag => tag.type === 'param')
+                        .reduce((prev, curr) => {
 
-                        prev.push({
-                            'name': curr.name.replace(/\[|\]/g, ''),
-                            'isOptional': curr.optional,
-                            'types': curr.types,
-                            'description': curr.description
-                        });
+                            prev.push({
+                                'name': curr.name.replace(/\[|\]/g, ''),
+                                'isOptional': curr.optional,
+                                'types': curr.types,
+                                'description': curr.description
+                            });
 
-                        return prev;
+                            return prev;
 
-                    }, []),
-                'property': curr.tags.filter(tag => tag.type === 'property')
-                    .reduce((prev, curr) => {
+                        }, []),
+                    'property': curr.tags.filter(tag => tag.type === 'property')
+                        .reduce((prev, curr) => {
 
-                        prev.push({
-                            'name': curr.name,
-                            'types': curr.types,
-                            'description': curr.description
-                        });
+                            prev.push({
+                                'name': curr.name,
+                                'types': curr.types,
+                                'description': curr.description
+                            });
 
-                        return prev;
+                            return prev;
 
-                    }, []),
-                'return': curr.tags.filter(tag => tag.type === 'return' || tag.type === 'returns')
-                    .reduce((prev, curr) => {
+                        }, []),
+                    'return': curr.tags.filter(tag =>
+                        tag.type === 'return' || tag.type === 'returns')
+                            .reduce((prev, curr) => {
 
-                        prev.push({
-                            'types': curr.types,
-                            'description': curr.description
-                        });
+                                prev.push({
+                                    'types': curr.types,
+                                    'description': curr.description
+                                });
 
-                        return prev;
+                                return prev;
 
-                    }, [])
-            }
-        });
+                            }, [])
+                }
+            });
 
-    }
+        }
 
-    return prev;
+        return prev;
 
-}, []);
+    }, []);
 
 module.exports = parser;
